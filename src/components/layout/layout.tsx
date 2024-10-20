2fc5b26d92572d9c3050980c57257009c93a9828
max <mcnic@users.noreply.github.com>
2024-10-20 01:34:51 -0700

    fix: layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,24 @@
 'use client'
 import { FC, ReactNode } from 'react'
+
+import styles from './layout.module.scss'
 import Header from '@/components/layout/header/header'
 import Sidebar from '@/components/layout/sidebar/sidebar'
-import styles from './layout.module.scss'
+import ButtonBack from '@/components/ui/ButtonBack/ButtonBack'
 
 type Props = {
   children: ReactNode
   sidebar?: boolean
   isSearch?: boolean
+  backButton?: boolean
 }
 
-const Layout: FC<Props> = ({ children, sidebar = true, isSearch = false }) => (
+const Layout: FC<Props> = ({
+  children,
+  sidebar = true,
+  isSearch = false,
+  backButton: showBackButton = false,
+}) => (
   <div className={styles.layout}>
     <div className={styles.container} style={{ flexDirection: 'column' }}>
       <Header isSearch={isSearch} />
@@ -21,6 +29,11 @@ const Layout: FC<Props> = ({ children, sidebar = true, isSearch = false }) => (
         </div>
       ) : (
         <div className={styles.layoutOne}>
+          {showBackButton && (
+            <div className={styles.left}>
+              <ButtonBack />
+            </div>
+          )}
           <div className={styles.center}>{children}</div>
         </div>
       )}
