c1c6139d3f7a29f9d2947c422ffd72a375c650a7
max <mcnic@users.noreply.github.com>
2024-12-01 16:59:58 -0800

    fix: tests errors on  null BASE_URL from .env file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import { loadEnv } from 'vite'
 
 export default defineConfig({
   test: {
+    env: loadEnv('', process.cwd(), ''),
     globals: true,
     coverage: {
       reporter: ['text', 'html'],
     },
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname, 
-    }
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
   },
 })
