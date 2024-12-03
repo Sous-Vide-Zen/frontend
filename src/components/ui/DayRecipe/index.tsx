fa6f5a89e6f3b72b3a73e4e23428c487f6ca1c4a
max <mcnic@users.noreply.github.com>
2024-12-02 23:21:08 -0800

    fix: DayRecipe

--- src/components/ui/DayRecipe/index.tsx
+++ src/components/ui/DayRecipe/index.tsx
@@ -1,15 +1,19 @@
-'use client'
-
 import { FC } from 'react'
-
 import styles from './DayRecipe.module.scss'
-import RecipePreview from '@/components/ui/RecipePreview'
 
-const DayRecipe: FC = () => {
+type Props = {
+  img?: string
+  username?: string
+}
+
+const DayRecipe: FC<Props> = () => {
   return (
     <div className={styles.topRecipe}>
       <h2>Рецепт дня</h2>
-      <RecipePreview />
+      <div className={styles.container}>
+        <div className={styles.zaglushka}>Фото отсутствует</div>
+        <span>Тыква с мёдом, чесноком, горчицей и лавровыми листами</span>
+      </div>
     </div>
   )
 }
