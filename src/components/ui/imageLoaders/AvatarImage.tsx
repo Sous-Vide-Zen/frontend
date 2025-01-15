510b53f48396566185db3bec411901067df61ab1
sergioivanov008 <sergioivanov008@gmail.com>
2025-01-15 23:59:15 +0300

    feat: add starting layout for set avatar logic

--- src/components/ui/imageLoaders/AvatarImage.tsx
+++ src/components/ui/imageLoaders/AvatarImage.tsx
@@ -1,17 +1,48 @@
+import Image from 'next/image'
 import styles from './avatarImage.module.scss'
 import { Button } from '@/components/ui'
 
-export const AvatarImage = () => {
+type Props = {
+  avatar: string | undefined
+}
+
+export const AvatarImage = ({ avatar }: Props) => {
+  const handleSetAvatar = () => {
+    console.log('handleSetAvatar');
+  }
+
   return (
     <div className={styles.container}>
-      <Button
+      {avatar
+        ? <div
+            className={styles.changeAvatar}
+            onClick={handleSetAvatar}
+          >
+            <Image
+              src={avatar}
+              priority={true}
+              width={150}
+              height={150}
+              alt="user avatar"
+              />
+            <div className={styles.text}>Изменить миниатюру</div>
+          </div>
+        : <Button
+            size="medium"
+            color="secondary"
+            onClick={handleSetAvatar}
+          >
+            Добавить фото +
+          </Button>
+      }
+      {/* <Button
         // className={styles.addPhoto}
         size="medium"
         color="secondary"
         // onClick={handlePick}
       >
         Добавить фото +
-      </Button>
+      </Button> */}
 
       {/* скрытый блок превью картинки */}
       {/* <div className={styles.previewPhoto}>
